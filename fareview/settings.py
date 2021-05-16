3b3905e3fa6ccdd93323ae64c9ad36a998113328
Jerry Ng <ngshiheng@gmail.com>
2021-05-16 09:21:15 +0800

    Add colourized log

--- fareview/settings.py
+++ fareview/settings.py
@@ -1,4 +1,38 @@
+import copy
 import os
+
+import scrapy.utils.log
+from colorlog import ColoredFormatter
+
+# Logging
+color_formatter = ColoredFormatter(
+    (
+        '%(log_color)s%(levelname)-5s%(reset)s '
+        '%(yellow)s[%(asctime)s]%(reset)s'
+        '%(white)s %(name)s %(funcName)s %(bold_purple)s:%(lineno)d%(reset)s '
+        '%(log_color)s%(message)s%(reset)s'
+    ),
+    datefmt='%d-%B-%y %H:%M:%S',
+    log_colors={
+        'DEBUG': 'blue',
+        'INFO': 'bold_cyan',
+        'WARNING': 'red',
+        'ERROR': 'bg_bold_red',
+        'CRITICAL': 'red,bg_white',
+    }
+)
+
+_get_handler = copy.copy(scrapy.utils.log._get_handler)
+
+
+def _get_handler_custom(*args, **kwargs):
+    handler = _get_handler(*args, **kwargs)
+    handler.setFormatter(color_formatter)
+    return handler
+
+
+scrapy.utils.log._get_handler = _get_handler_custom
+
 
 BOT_NAME = 'fareview'
 
