b65ca70ae54157f9c92b8a4c4b96795bf113c951
Jerry Ng <ngshiheng@gmail.com>
2021-05-31 23:50:00 +0800

    Configure DB session

--- fareview/extensions.py
+++ fareview/extensions.py
@@ -7,7 +7,10 @@ from scrapy.exceptions import NotConfigured
 from scrapy.spiders import Spider
 from scrapy.utils.project import get_project_settings
 from sentry_sdk.integrations.sqlalchemy import SqlalchemyIntegration
+from sqlalchemy.orm import sessionmaker
 from telegram import Bot
+
+from fareview.models import User, db_connect
 
 logger = logging.getLogger(__name__)
 
@@ -50,7 +53,14 @@ class TelegramBot:
         if not telegram_api_token:
             raise NotConfigured('TELEGRAM_API_TOKEN is not configured.')
 
+        engine = db_connect()
+
+        self.session = sessionmaker(bind=engine)
+
         self.bot = Bot(token=telegram_api_token)
+
+    def _create_notification_payload(self) -> str:
+        return ''
 
     @classmethod
     def from_crawler(cls, crawler: Crawler) -> 'TelegramBot':
@@ -71,5 +81,12 @@ class TelegramBot:
     def spider_closed(self, spider: Spider) -> None:
         logger.info(f'Spider closed: {spider.name}')
 
-        # TODO: Search through the entire user list and send out notifications
-        self.bot.send_message(chat_id='', text='')
+        session = self.session()
+        users = session.query(User).filter(User.membership_end_date.between(User.membership_start_date, User.membership_end_date)).all()
+
+        logger.info(f'Sending Telegram notifications to {len(users)} users.')
+        session.close()
+
+        for user in users:
+            # TODO: Search through the entire user list and send out notifications
+            self.bot.send_message(chat_id=user.telegram_chat_id, text='Hi!')
