74eba538fa786d8d7865b3a0f159a44673376d72
Jerry Ng <ngshiheng@gmail.com>
2021-06-02 12:07:29 +0800

    Retry deployment

--- app.py
+++ app.py
@@ -200,12 +200,11 @@ def main() -> None:
 
     # Start the Bot
     updater.start_webhook(
-        listen="0.0.0.0",
+        listen='0.0.0.0',
         port=8443,
         url_path=TELEGRAM_API_TOKEN,
+        webhook_url='https://fareview.herokuapp.com/' + TELEGRAM_API_TOKEN,
     )
-
-    updater.bot.setWebhook('https://fareview.herokuapp.com/' + TELEGRAM_API_TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT, SIGTERM or SIGABRT
     # This should be used most of the time, since start_polling() is non-blocking and will stop the bot gracefully
