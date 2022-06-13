074be7f5fc9b142b5834a399cdfbc70e47337c4d
Jerry Ng <ngshiheng@gmail.com>
2022-06-13 21:37:29 +0800

    chore(scripts): add script to generate csv data for each brand

---
+++ scripts/generate_csv.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+BRANDS=("carlsberg" "tiger" "heineken" "guinness" "asahi")
+
+PGDATABASE="${PGDATABASE-fareview}"
+PGHOST="${PGHOST-localhost}"
+PGPASSWORD="${PGPASSWORD-}"
+PGPORT="${PGPORT-5432}"
+PGUSER="${PGUSER-postgres}"
+
+mkdir -p data/
+for brand in "${BRANDS[@]}"; do
+    PGPASSWORD=$PGPASSWORD psql -v brand="'${brand}'" -h "$PGHOST" -U "$PGUSER" -d "$PGDATABASE" -F ',' -A -f alembic/examples/get_all_by_brand.sql >"data/${brand}.csv"
+done
